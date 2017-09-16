d9b5f87c3d8d839f8343042451c228407f0b98de
Anthony Palmer <anthony@motivislearning.com>
2017-09-16 13:51:25 -0400

    fix error with replaying in same channel

--- modules/soundboard.js
+++ modules/soundboard.js
@@ -6,6 +6,20 @@ module.exports = (bot) => {
   bot.on('message', (user, userID, channelID, message, event) => {
     if (message.startsWith('$')) {
       try {
+        // try to join correct channel
+        bot.joinVoiceChannel(channelID, (error, events) => {
+          if (error) return console.error(error)
+        })
+      } catch (e) {
+        if (e.message.includes('Voice channel already active')) {
+          // everythings fine
+        } else {
+          console.log(e)
+          return
+        }
+      }
+
+      try {
         playSound(bot.getVoiceChannel(event), _.replace(message, '$', ''))
       } catch (e) {
         console.log(e)
@@ -14,36 +28,32 @@ module.exports = (bot) => {
   })
 
   function playSound (channelID, filename) {
-    // Let's join the voice channel, the ID is whatever your voice channel's ID is.
-    bot.joinVoiceChannel(channelID, function (error, events) {
-      // Check to see if any errors happen while joining.
+    // Then get the audio context
+    bot.getAudioContext(channelID, (error, stream) => {
+      // Once again, check to see if any errors exist
       if (error) return console.error(error)
 
-      // Then get the audio context
-      bot.getAudioContext(channelID, function (error, stream) {
-        // Once again, check to see if any errors exist
-        if (error) return console.error(error)
+      // Create a stream to your file and pipe it to the stream
+      // Without {end: false}, it would close up the stream, so make sure to include that.
+      console.log('Playing sound:', filename)
+      let read = fs.createReadStream(
+        path.join(`/home/pi/node/brave-bot/soundFiles`, `${filename}.ogg`))
+      console.log('Stream made', __dirname)
 
-        // Create a stream to your file and pipe it to the stream
-        // Without {end: false}, it would close up the stream, so make sure to include that.
-        console.log('Playing sound:', filename)
-        let read = fs.createReadStream(
-          path.join(`/home/pi/node/brave-bot/soundFiles`, `${filename}.ogg`))
-        console.log('Stream made', __dirname)
+      read.on('open', () => {
+        console.log('in open event', __dirname)
+        read.pipe(stream, {
+          end: false
+        })
+        console.log('after pipe')
+      })
+      // The stream fires `done` when it's got nothing else to send to Discord.
+      stream.on('done', () => {
+        // Handle
+      })
 
-        read.on('open', () => {
-          console.log('in open event', __dirname)
-          read.pipe(stream, {end: false})
-          console.log('after pipe')
-        })
-        // The stream fires `done` when it's got nothing else to send to Discord.
-        stream.on('done', function () {
-          // Handle
-        })
-
-        stream.on('error', (error) => {
-          console.log(error)
-        })
+      stream.on('error', (error) => {
+        console.log(error)
       })
     })
   }
