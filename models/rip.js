c762807f5494161e1ce9e252f96ec802930ea4a2
Anthony Palmer <anthony@motivislearning.com>
2017-09-30 16:12:27 -0400

    awdw

--- models/rip.js
+++ models/rip.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 const _ = require('lodash')
 // const db = require('../db')
 const pg = require('pg')
+const path = require('path')
 
 fs.readdir(`/home/pi/node/brave-bot/soundFiles`, async (err, files) => {
   if (err) console.log(err)
 
   files = files.map(file => _.replace(file, '.ogg', ''))
 
-  let db = new pg.Client()
-  await db.connect()
-  files.forEach(async (elem) => {
-    console.log(await s.create(elem, db))
+  files.forEach((elem) => {
+    let stats = fs.statSync(path.join(`/home/pi/node/brave-bot/soundFiles`, `${elem}.ogg`))
+    console.log(stats.birthtime)
   })
+
+  // let db = new pg.Client()
+  // await db.connect()
+  // files.forEach(async (elem) => {
+  //   console.log(await s.create(elem, db))
+  // })
 })
