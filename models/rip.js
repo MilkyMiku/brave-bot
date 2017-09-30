d06195e70d6050f501d55b41b988ee30a2b3ccd9
Anthony Palmer <anthony@motivislearning.com>
2017-09-30 15:52:52 -0400

    awd

--- models/rip.js
+++ models/rip.js
@@ -1,13 +1,15 @@
 let s = require('./sound')
 const fs = require('fs')
 const _ = require('lodash')
-const db = require('../db')
+// const db = require('../db')
+const pg = require('pg')
 
 fs.readdir(`/home/pi/node/brave-bot/soundFiles`, async (err, files) => {
   if (err) console.log(err)
 
   files = files.map(file => _.replace(file, '.ogg', ''))
 
+  let db = new pg.Client()
   await db.connect()
   files.forEach(async (elem) => {
     console.log(await s.create(elem, db))
